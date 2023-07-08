fd75def31886e2fb4e81ef9ce156bd71a0e0d14f
Jann Stute <jann.stute@protonmail.com>
2023-07-08 02:42:23 +0200

    prevent some duplicate output

--- csgo_combat_log.py
+++ csgo_combat_log.py
@@ -9,15 +9,45 @@ OUT_TAKEN = "Damage Taken from \"{0}\" - {1} in {2} hit"
 URL = "localhost"
 PORT = 2121
 
+current_line = 0
+temp = False
+last_log = []
+
+def print_entry(entry: tuple[int,str,int,int]):
+    template = OUT_TAKEN
+    color = colored.red
+    if entry[0] == 2:
+        template = OUT_GIVEN
+        color = colored.green
+    print(template.format(entry[1], color(entry[2]), color(entry[3])))
+
+def print_all_entries():
+    for e in last_log:
+        print_entry(e)
 
 def handle_line(line: str):
+    global current_line
+    global last_log
+    global temp
     m = match_line(line)
     if m[0] == 1:
+        current_line = 0
         print(OUT_HEADER)
-    elif m[0] == 2:
-        print(OUT_GIVEN.format(m[1], colored.green(m[2]), colored.green(m[3])))
-    elif m[0] == 4:
-        print(OUT_TAKEN.format(m[1], colored.red(m[2]), colored.red(m[3])))
+    elif m[0] in [2,4]:
+        if current_line < len(last_log):
+            if last_log[current_line] != m:
+                last_log = last_log[:current_line+1]
+                last_log[current_line] = m
+                temp = True
+                print_all_entries()
+        else:
+            if not temp:
+                temp = True
+                print_all_entries()
+            last_log.append(m)
+            print_entry(m)
+        current_line += 1
+
 
 async def output_parser(reader: telnetlib3.TelnetReader, writer: telnetlib3.TelnetWriter):
     print("connected")
